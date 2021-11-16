d655e6bac8227c033edb12ccde020c9ed5d056e9
CommanderFoo <pixeldepth@gmail.com>
2021-11-16 11:14:47 +0000

    Update ServerCommands.lua

--- Data/Scripts/ServerCommands.lua
+++ Data/Scripts/ServerCommands.lua
@@ -375,22 +375,3 @@ CommandParser.AddCommand("help", function(sender, params, status)
 		status.senderMessage = CommandParser.error.NO_PERMISSION
 	end
 end)
-
-
--- /setteam player team
--- CommandParser.AddCommand("setteam", function(sender, params, status)
--- 	if CommandParser.HasPermission(sender, CommandParser.permissions.MODERATOR) then
--- 		local player = CommandParser.GetPlayer(params[2])
-
--- 		if player ~= nil then
--- 			player.team = tonumber(params[3]) or player.team
-
--- 			status.success = true
--- 			status.senderMessage = "Player team set."
--- 		else
--- 			status.senderMessage = CommandParser.error.INVALID_PLAYER
--- 		end
--- 	else
--- 		status.senderMessage = CommandParser.error.NO_PERMISSION
--- 	end
--- end)
